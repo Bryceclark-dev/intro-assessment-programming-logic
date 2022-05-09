40c80df1e0b0ae3971da54c4ce810c949c7614e3
Bryceclark-dev <brycejclark@sbcglobal.net>
2022-05-09 02:51:18 -0500

    Formatted code

--- src/main/java/com/kenzie/app/Main.java
+++ src/main/java/com/kenzie/app/Main.java
@@ -6,53 +6,53 @@ public class Main {
 
     public static void main(String[] args) {
         // Do not directly modify this array!
-        final int[] numbers = new int[]{ 469, 755, 244, 245, 758, 450, 302, 20, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472 };
+        final int[] numbers = new int[]{469, 755, 244, 245, 758, 450, 302, 20, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472};
 
         System.out.println("****Array Analyzer Program****");
-	    // write your code here
+        // write your code here
         int largest = 0;
         int smallest = numbers[0];
         int sum = 0;
-        double average = 0;
+        double average;
         int countEven = 0;
         int countOdd = 0;
         int countEight = 0;
         int[] evenArr, oddArr, divEight;
 
         // You can create variables to store each result, for example: int largestNumber = 0;
-        for(int num : numbers){
-            if(num > largest){
+        for (int num : numbers) {
+            if (num > largest) {
                 largest = num;
             }
-            if(num < smallest){
+            if (num < smallest) {
                 smallest = num;
             }
-            if(num % 2 == 0){
+            if (num % 2 == 0) {
                 countEven++;
-            }else {
+            } else {
                 countOdd++;
             }
-            if (num % 8 == 0){
+            if (num % 8 == 0) {
                 countEight++;
             }
             sum = sum + num;
 
         }
-        average = (double)sum/ numbers.length;
+        average = (double) sum / numbers.length;
         evenArr = new int[countEven];
         oddArr = new int[countOdd];
         divEight = new int[countEight];
         countEven = countOdd = countEight = 0;
 
-        for(int num : numbers){
-            if(num % 2 == 0 ){
+        for (int num : numbers) {
+            if (num % 2 == 0) {
                 evenArr[countEven] = num;
                 countEven++;
-            }else{
+            } else {
                 oddArr[countOdd] = num;
                 countOdd++;
             }
-            if(num % 8 == 0){
+            if (num % 8 == 0) {
                 divEight[countEight] = num;
                 countEight++;
             }
@@ -60,7 +60,7 @@ public class Main {
 
 
         System.out.println("Largest Number: " + largest);
-        System.out.println("Smallest Number: " + smallest );
+        System.out.println("Smallest Number: " + smallest);
         System.out.println("Sum: " + sum);
         System.out.println("Average: " + average);
         System.out.println("Even Numbers: " + Arrays.toString(evenArr));
